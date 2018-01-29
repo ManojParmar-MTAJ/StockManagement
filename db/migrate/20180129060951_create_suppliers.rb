5125a59c164003c86cb7aeea6b52feee4bf0f5e6
dharmik <email_id>
2018-01-29 17:18:56 +0530

    supplier crud wip

---
+++ db/migrate/20180129060951_create_suppliers.rb
@@ -0,0 +1,17 @@
+class CreateSuppliers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :suppliers do |t|
+      t.string :supplier_name
+      t.string :product_name
+      t.string :company_name
+      t.string :address
+      t.string :date
+      t.string :city_name
+      t.string :contact_no
+      t.string :gst_no
+
+
+      t.timestamps
+    end
+  end
+end
