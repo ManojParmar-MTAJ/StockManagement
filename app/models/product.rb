f89c0cea487dcd35211ab317bc62a7e44eb2266d
user_name <email_id>
2018-01-30 17:15:50 +0530

    add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :product_name,:product_code,:brand,:product_category,:unit, presence: true
+  validates :cgst,:sgst,:product_cost,:product_price, numericality:true
+  validates :product_code, length: { maximum: 4}
+  validates :product_name, length: { minimum: 2 }
 end
