09cb7a42f025c80eefe623284b392b09fd81019f
RaviMariya-MTAJ <ravi.mariya@mtajsolutions.com>
2018-03-21 14:47:29 +0530

    fixed string data type of customer id and product id in sales

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180312084216) do
   create_table "products", force: :cascade do |t|
     t.string "product_name"
     t.string "product_code"
+    t.string "image"
     t.string "brand"
     t.string "product_category"
     t.string "unit"
@@ -44,8 +45,8 @@ ActiveRecord::Schema.define(version: 20180312084216) do
   end
 
   create_table "sales", force: :cascade do |t|
-    t.string "customer_id"
-    t.string "product_id"
+    t.integer "customer_id"
+    t.integer "product_id"
     t.string "payment_status"
     t.string "sales_date"
     t.datetime "created_at", null: false
