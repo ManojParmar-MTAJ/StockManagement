f8bfa24d6109d881c2f7ca46a45d455a2d8d2929
RaviMariya-MTAJ <ravi.mariya@mtajsolutions.com>
2018-03-22 16:09:13 +0530

    dashboard: created graph for sales, and added dynamic data in stats

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,9 @@
 class HomesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    total_sales = Sale.all.sum{|sale| sale.unit * sale.product.product_price}
+    total_sales_return = SalesReturn.all.sum{|sales_return| sales_return.sale.product.product_price}
+    @stats = {users_count: Customer.count, sales_count: helpers.number_to_rupees(total_sales), sales_return_count: helpers.number_to_rupees(total_sales_return)}
   end
 end
--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,10 @@
 module HomesHelper
+  def sales_graph
+    sales_data = (2010..2020).collect{|year| {year: year.to_s, total_sales: sales_per_year(year)}}
+    sales_data = sales_data.delete_if{|k| k[:total_sales] <= 0}.to_json.html_safe
+  end
+
+  def sales_per_year(year)
+    Sale.all.select{|sale| sale.sales_date.include?(year.to_s)}.sum{|sale| sale.unit * sale.product.product_price}
+  end
 end
